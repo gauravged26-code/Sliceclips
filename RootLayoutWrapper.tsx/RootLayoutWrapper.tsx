64f03655bffb6b3a7ba8051c5e1a3af8b9e463f0
gauravged26-code <gauravged26@gmail.com>
2026-06-23 01:36:10 +0545

    Update RootLayoutWrapper.tsx

--- RootLayoutWrapper.tsx/RootLayoutWrapper.tsx
+++ RootLayoutWrapper.tsx/RootLayoutWrapper.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { auth } from "../lib/firebase";
+import { auth } from "lib/firebase";
 import { onAuthStateChanged, User } from "firebase/auth";
 // Import your template's original Navbar component here
 // Note: Adjust this import path to match where your template's Navbar actually lives 
