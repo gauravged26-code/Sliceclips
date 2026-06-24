e28eef7552596700e664c46de75ba8d089b4ae05
gauravged26-code <gauravged26@gmail.com>
2026-06-25 00:37:13 +0545

    Update next.config.js

--- files/next.config.js
+++ files/next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline';",
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com;",
           },
         ],
       },
