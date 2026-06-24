d278e7395d93ffe75cc01b1992c539becd289faa
gauravged26-code <gauravged26@gmail.com>
2026-06-24 23:43:25 +0545

    Add headers to bypass Content Security Policy

--- files/next.config.js
+++ files/next.config.js
@@ -6,7 +6,21 @@ const nextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+  // Add this headers block to bypass the CSP block
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline';",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
